aec62614c1092999f49b7f52f4dfba6903d5ac89
fred3466 <fbourgue@student.42perpignan.fr>
2024-02-22 13:39:48 +0100

    ajout des premières méthodes de l'interface

--- src/request/Request.h
+++ src/request/Request.h
@@ -9,5 +9,8 @@ public:
 	~Request();
 //	Request(Request const &o);
 //	Request& operator=(Request const &o);
+
+	std::string getValue(std::string paramName);
+	void addParam(std::string paramName, std::string paramValue);
 };
 
