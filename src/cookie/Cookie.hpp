adbb9093eccd94821da97e39ff9ef2aeab7b662a
Olskor <jbamine34@gmail.com>
2024-03-13 15:07:54 +0100

    implementation class cookie

---
+++ src/cookie/Cookie.hpp
@@ -0,0 +1,35 @@
+#include "../request/RequestHttpHeader.h"
+#include <ctime>
+#include <iostream>
+#include <iomanip>
+
+class Cookie
+{
+private:
+	std::time_t _maxAge;
+	std::string _path;
+	std::string _domain;
+	std::string _secure;
+	std::string _httponly;
+	std::string _name;
+	std::string _value;
+public:
+	Cookie();
+	~Cookie();
+	long getMaxAge() const;
+	std::string getPath() const;
+	std::string getDomain() const;
+	std::string getSecure() const;
+	std::string getHttponly() const;
+	std::string getName() const;
+	std::string getValue() const;
+	void setMaxAge(long &max_age);
+	void setPath(const std::string &path);
+	void setDomain(const std::string &domain);
+	void setSecure();
+	void setHttponly();
+	void setName(const std::string &name);
+	void setValue(const std::string &value);
+	std::string getCookie() const;
+	void dump() const;
+};
