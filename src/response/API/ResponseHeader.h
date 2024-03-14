4ac5579eeef35165194a342f0248c3ae8617756f
Olskor <jbamine34@gmail.com>
2024-03-14 11:49:48 +0100

    class cookie dans response

--- src/response/API/ResponseHeader.h
+++ src/response/API/ResponseHeader.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <string>
 #include <list>
+#include "../../cookie/Cookie.h"
 
 class ResponseHeader
 {
@@ -14,5 +15,8 @@ public:
 	virtual const std::list<std::string>& getFields() const =0;
 	virtual const std::string& getStatusLine() const =0;
 	virtual void setStatusLine(const std::string &statusLine)=0;
+	virtual Cookie getCookie(const std::string &cookieName)=0;
+	virtual bool addCookie(const Cookie &cookie)=0;
+	virtual bool removeCookie(const std::string &cookieName)=0;
 };
 
--- src/response/ResponseHttpHeader.h
+++ src/response/ResponseHttpHeader.h
@@ -2,12 +2,16 @@
 #include <string>
 #include <list>
 #include "API/ResponseHeader.h"
+#include "../cookie/Cookie.h"
+#include "../cookie/CookieHelper.h"
 
 class ResponseHttpHeader: public ResponseHeader
 {
 private:
 	std::string statusLine;
 	std::list<std::string> fields;
+	std::list<Cookie> cookies;
+	CookieHelper cookieHelper;
 
 public:
 	ResponseHttpHeader();
@@ -17,5 +21,8 @@ public:
 	virtual const std::list<std::string>& getFields() const;
 	virtual const std::string& getStatusLine() const;
 	virtual void setStatusLine(const std::string &statusLine);
+	virtual Cookie getCookie(const std::string &cookieName);
+	virtual bool addCookie(const Cookie &cookie);
+	virtual bool removeCookie(const std::string &cookieName);
 };
 
