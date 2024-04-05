252cc4eedba9e5bca1939c7b8bf26d7f55601249
anshazet <anshazet@gmail.com>
2024-04-05 12:00:45 +0200

    menage

--- src/processor/FiltreRequestPost.cpp
+++ src/processor/FiltreRequestPost.cpp
@@ -52,52 +52,6 @@ Response *FiltreRequestPost::process(Request *request, Response *response, Proce
 	return response;
 }
 
-// Response *FiltreRequestPost::process(Request *request, Response *response, ProcessorAndLocationToProcessor *processorAndLocationToProcessor)
-// {
-// 	(void)processorAndLocationToProcessor;
-// 	RequestBody *body = request->getBody();
-// 	(void)body;
-
-// 	std::string CONTENT_TYPE = request->getHeaderFieldValue("Content-Type");
-// 	std::string CONTENT_LENGTH = request->getHeaderFieldValue("Content-Length");
-
-// 	// Debug: Print the Content-Type and Content-Length
-// 	std::cout << "Debug: CONTENT_TYPE = " << CONTENT_TYPE << std::endl;
-// 	std::cout << "Debug: CONTENT_LENGTH = " << CONTENT_LENGTH << std::endl;
-
-// 	// Convert CONTENT_LENGTH to integer
-// 	int contentLength = StringUtil().intFromStr(CONTENT_LENGTH);
-
-// 	// Debug: Print the parsed content length
-// 	std::cout << "Debug: Parsed contentLength = " << contentLength << std::endl;
-
-// 	// Retrieve the maximum body size from the configuration
-// 	int maxBodySize = config->getParamInt("max_body_size", -1); // Default to -1 if not set
-
-// 	// Debug: Print the max body size
-// 	std::cout << "Debug: maxBodySize from config = " << maxBodySize << std::endl;
-
-// 	// Check if the content length exceeds the maximum body size
-// 	if (maxBodySize != -1 && contentLength > maxBodySize)
-// 	{
-// 		// Debug: Print a message if the content length exceeds max body size
-// 		std::cout << "Debug: Content length exceeds max body size, rejecting request." << std::endl;
-
-// 		// Content length exceeds maximum, reject the request
-// 		HttpError *error = HttpErrorFactory().build(413); // 413 Payload Too Large
-// 		response->setHttpError(error);
-
-// 		// Debug: Print that we're setting a 413 error
-// 		std::cout << "Debug: Setting 413 Payload Too Large error in response." << std::endl;
-// 		return response;
-// 	}
-
-// 	// Debug: If request is processed without exceeding max body size
-// 	std::cout << "Debug: Request processed successfully without exceeding max body size." << std::endl;
-
-// 	return response;
-// }
-
 void FiltreRequestPost::addProperty(std::string name, std::string value)
 {
 	config->addParam(name, value);
